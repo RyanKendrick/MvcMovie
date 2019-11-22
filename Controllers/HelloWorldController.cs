6aca9443fe198ab4c9f3ddc133beca06e6d753ff
RyanKendrick <rpkendrick@gmail.com>
2019-11-22 16:01:35 -0500

    added Index.cshtml and Welcome.cshtml Views

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -5,20 +5,17 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        //
-        // GET: /HelloWorld/
-        // Every public method in a controller is callable as an HTTP endpoint
         public IActionResult Index()
         {
-            // code calls the controller's View method. It uses a view template to generate an HTML response.
             return View();
         }
 
-        // GET: /HelloWorld/Welcome/
-        // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int ID = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
